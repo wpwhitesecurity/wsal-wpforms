0f4c7753675d6cb453d9d7b6bdeb71897ae6bda4
Danny Jones <59394454+DannyWPWS@users.noreply.github.com>
2021-04-22 05:07:48 +0100

    Updating event text (#103)
    Fixes #102

--- wp-security-audit-log/custom-alerts.php
+++ wp-security-audit-log/custom-alerts.php
@@ -8,9 +8,8 @@ $custom_alerts = [
                 5500,
                 WSAL_LOW,
                 __( 'A form was created, modified or deleted', 'wsal-wpforms' ),
-                __( 'A form was created, modified or deleted', 'wsal-wpforms' ),
+                __( 'The Form called %PostTitle%.', 'wsal-wpforms' ),
                 [
-                    __( 'Form name', 'wsal-wpforms' ) => '%PostTitle%',
                     __( 'Form ID', 'wsal-wpforms' )   => '%PostID%',
                 ],
                 [
@@ -24,10 +23,8 @@ $custom_alerts = [
                 5501,
                 WSAL_MEDIUM,
                 __( 'A field was created, modified or deleted from a form.', 'wsal-wpforms' ),
-                __( 'A field was created, modified or deleted from a form.', 'wsal-wpforms' ),
+                __( 'The Field called %field_name% in the form %form_name%.', 'wsal-wpforms' ),
                 [
-                    __( 'Field name', 'wsal-wpforms' ) => '%field_name%',
-                    __( 'Form name', 'wsal-wpforms' )  => '%form_name%',
                     __( 'Form ID', 'wsal-wpforms' )    => '%PostID%',
                 ],
                 [
@@ -41,9 +38,8 @@ $custom_alerts = [
                 5502,
                 WSAL_MEDIUM,
                 __( 'A form was duplicated', 'wsal-wpforms' ),
-                __( 'Form <strong>%OldPostTitle%</strong> was duplicated', 'wsal-wpforms' ),
+                __( 'Duplicated the form %OldPostTitle%.', 'wsal-wpforms' ),
                 [
-                    __( 'Source form', 'wsal-wpforms' )    => '%OldPostTitle%',
                     __( 'Source form ID', 'wsal-wpforms' ) => '%SourceID%',
                     __( 'New form ID', 'wsal-wpforms' )    => '%PostID%',
                 ],
@@ -58,10 +54,8 @@ $custom_alerts = [
                 5503,
                 WSAL_LOW,
                 __( 'A notification was added to a form, enabled or modified', 'wsal-wpforms' ),
-                __( 'A notification was added to a form, enabled or modified', 'wsal-wpforms' ),
+                __( 'The Notification called %notifiation_name% in the form %form_name%.', 'wsal-wpforms' ),
                 [
-                    __( 'Notification name', 'wsal-wpforms' ) => '%notifiation_name%',
-                    __( 'Form name', 'wsal-wpforms' ) => '%form_name%',
                     __( 'Form ID', 'wsal-wpforms' )   => '%PostID%',
                 ],
                 [
@@ -75,9 +69,8 @@ $custom_alerts = [
                 5504,
                 WSAL_MEDIUM,
                 __( 'An entry was deleted', 'wsal-wpforms' ),
-                __( 'An entry was deleted', 'wsal-wpforms' ),
+                __( 'Deleted the Entry with the email address %entry_email%.', 'wsal-wpforms' ),
                 [
-                    __( 'Entry email address', 'wsal-wpforms' ) => '%entry_email%',
                     __( 'Entry ID', 'wsal-wpforms' )  => '%entry_id%',
                     __( 'Form name', 'wsal-wpforms' ) => '%form_name%',
                     __( 'Form ID', 'wsal-wpforms' )   => '%form_id%',
@@ -93,9 +86,8 @@ $custom_alerts = [
                 5505,
                 WSAL_LOW,
                 __( 'Notifications were enabled or disabled in a form', 'wsal-wpforms' ),
-                __( 'Notifications were enabled or disabled in a form', 'wsal-wpforms' ),
+                __( 'Changed the status of all the notifications in the form %form_name%.', 'wsal-wpforms' ),
                 [
-                    __( 'All the notifications in the form ', 'wsal-wpforms' ) => '%form_name%',
                     __( 'Form ID', 'wsal-wpforms' ) => '%PostID%',
                 ],
                 [
@@ -109,10 +101,8 @@ $custom_alerts = [
                 5506,
                 WSAL_LOW,
                 __( 'A form was renamed', 'wsal-wpforms' ),
-                __( 'A form was renamed', 'wsal-wpforms' ),
+                __( 'Renamed the form %old_form_name% to %new_form_name%.', 'wsal-wpforms' ),
                 [
-                    __( 'Previous form name', 'wsal-wpforms' ) => '%old_form_name%',
-                    __( 'New form name', 'wsal-wpforms' ) => '%new_form_name%',
                     __( 'Form ID', 'wsal-wpforms' )       => '%PostID%',
                 ],
                 [
@@ -126,9 +116,8 @@ $custom_alerts = [
                 5507,
                 WSAL_MEDIUM,
                 __( 'An entry was modified', 'wsal-wpforms' ),
-                __( 'An entry was modified', 'wsal-wpforms' ),
+                __( 'Modified the Entry with ID %entry_id%.', 'wsal-wpforms' ),
                 [
-                    __( 'Entry ID', 'wsal-wpforms' )       => '%entry_id%',
                     __( 'From form', 'wsal-wpforms' )      => '%form_name%',
                     __( 'Modified field name', 'wsal-wpforms' ) => '%field_name%',
                     __( 'Previous value', 'wsal-wpforms' ) => '%old_value%',
@@ -145,9 +134,8 @@ $custom_alerts = [
                 5508,
                 WSAL_HIGH,
                 __( 'Plugin access settings were changed', 'wsal-wpforms' ),
-                __( 'Plugin access settings were changed', 'wsal-wpforms' ),
+                __( 'Changed the WPForms access setting %setting_name%.', 'wsal-wpforms' ),
                 [
-                    __( 'Access setting', 'wsal-wpforms' ) => '%setting_name%',
                     __( 'Type', 'wsal-wpforms' )           => '%setting_type%',
                     __( 'Previous privileges', 'wsal-wpforms' ) => '%old_value%',
                     __( 'New privileges', 'wsal-wpforms' ) => '%new_value%',
@@ -161,10 +149,9 @@ $custom_alerts = [
                 5509,
                 WSAL_HIGH,
                 __( 'Currency settings were changed', 'wsal-wpforms' ),
-                __( 'Changed the <strong>currency</strong>', 'wsal-wpforms' ),
+                __( 'Changed the <strong>currency</strong> to %new_value%.', 'wsal-wpforms' ),
                 [
                     __( 'Previous currency', 'wsal-wpforms' ) => '%old_value%',
-                    __( 'New currency', 'wsal-wpforms' ) => '%new_value%',
                 ],
                 [],
                 'wpforms',
@@ -174,10 +161,9 @@ $custom_alerts = [
             [
                 5510,
                 WSAL_HIGH,
-                __( 'A service integration was added or deleted.', 'wsal-wpforms' ),
-                __( 'A service integration was added or deleted.', 'wsal-wpforms' ),
+                __( 'A service integration was added or deleted', 'wsal-wpforms' ),
+                __( 'A service integration with %service_name%.', 'wsal-wpforms' ),
                 [
-                    __( 'Service', 'wsal-wpforms' ) => '%service_name%',
                     __( 'Connection name', 'wsal-wpforms' ) => '%connection_name%',
 					__( 'Service', 'wsal-wpforms' ) => '%service_name%',
                 ],
@@ -190,10 +176,8 @@ $custom_alerts = [
                 5511,
                 WSAL_HIGH,
                 __( 'An addon was installed, activated or deactivated.', 'wsal-wpforms' ),
-                __( 'An addon was installed, activated or deactivated.', 'wsal-wpforms' ),
-                [
-                    __( 'The addon', 'wsal-wpforms' ) => '%addon_name%',
-                ],
+                __( 'The addon %addon_name%.', 'wsal-wpforms' ),
+                [],
                 [],
                 'wpforms',
                 'activated',
