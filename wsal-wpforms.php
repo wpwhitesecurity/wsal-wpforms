d82343751d147f2cc586a8369d4e1b3f7e9ea967
Danny Jones <59394454+DannyWPWS@users.noreply.github.com>
2022-06-14 17:07:43 +0100

    Changelog + Version bump

--- wsal-wpforms.php
+++ wsal-wpforms.php
@@ -6,7 +6,7 @@
  * Text Domain: wsal-wpforms
  * Author: WP White Security
  * Author URI: http://www.wpwhitesecurity.com/
- * Version: 1.2.0
+ * Version: 1.2.1
  * License: GPL2
  * Network: true
  *
