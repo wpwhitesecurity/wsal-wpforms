71ba33524eb326f23285628fa376aa27fde90856
Robert Abela <robert@wpwhitesecurity.com>
2023-03-23 15:45:53 +0100

    UIpdated copyright year

--- wsal-wpforms.php
+++ wsal-wpforms.php
@@ -17,7 +17,7 @@
 use WSAL\Helpers\Classes_Helper;
 
 /*
-	Copyright(c) 2022  WP White Security  (email : info@wpwhitesecurity.com)
+	Copyright(c) 2023  WP White Security  (email : info@wpwhitesecurity.com)
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License, version 2, as
